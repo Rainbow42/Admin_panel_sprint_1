93e0c05d0c2811d18d231eddb8242c1504dd2267
antonida <a.medvedeva@mediasoft.team>
2021-09-17 23:51:59 +0400

    Project movies

--- movies_admin/movies_project/movies/admin.py
+++ movies_admin/movies_project/movies/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
 
+from movies.models import Filmwork, Genre
 
-class EntryAdmin(admin.ModelAdmin):
+
+class FikmworkAdmin(admin.ModelAdmin):
     list_display = (
-        'title', 'description', 'rating', 'genre', 'age_censor', 'actors', 'file_path'
+        'title', 'description', 'rating', 'age_censor', 'file_path'
     )
-    list_editable = ('genre', 'actors')
-    # list_filter = ('flag', 'blogs',)
-    # list_select_related = ('author',)
-    # search_fields = ('title', 'author__username')
-    # readonly_fields = ('updated_at', 'created_at')
-    # raw_id_fields = ('author',)
-    # filter_horizontal = ('blogs',)
-    # actions = (
-    #     'add_entries_to_search_index',
-    #     'delete_entries_to_search_index',
-    #     'delete_search_index'
-    # )
+    list_filter = ('rating', 'age_censor', 'created_at')
+    search_fields = ('title', )
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'description'
+    )
+    search_fields = ('name', )
+
+
+admin.site.register(Filmwork, FikmworkAdmin)
+admin.site.register(Genre, GenreAdmin)
--- movies_admin/movies_project/users/models.py
+++ movies_admin/movies_project/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.postgres.fields import ArrayField
 from django.core import validators
 from django.db import models
 from django.utils import timezone
@@ -7,7 +7,8 @@ from django.utils import timezone
 
 class Users(AbstractBaseUser):
     username = models.CharField(
-        'Имя пользователя', max_length=150,
+        'Имя пользователя',
+        max_length=150,
         unique=True,
         help_text='Обязательное. Не более 150 символов.',
         validators=[
@@ -22,16 +23,40 @@ class Users(AbstractBaseUser):
         ],
         error_messages={
             'unique': 'Пользователь с таким именем уже существует',
-        }
+        },
+        db_index=True
     )
-    first_name = models.CharField('Имя', max_length=250, blank=True, db_index=True)
-    last_name = models.CharField('Фамилия', max_length=250, blank=True)
-    email = models.EmailField('Email', max_length=250)
-    created_at = models.DateTimeField(verbose_name='Дата создания', blank=True,
-                                      default=timezone.now)
+    first_name = models.CharField(
+        'Имя',
+        max_length=250,
+        blank=True
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=250,
+        blank=True
+    )
+    email = models.EmailField(
+        'Email',
+        max_length=250
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Дата создания',
+        blank=True,
+        default=timezone.now
+    )
+    permissions = ArrayField(
+        models.CharField(max_length=255, blank=True),
+        size=8,
+        blank=True,
+        default=list
+    )
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return f"{self.username}"
 
     class Meta:
         verbose_name = 'user'
